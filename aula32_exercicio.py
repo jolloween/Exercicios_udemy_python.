195bdc547965a15fd3815098f120b4478a2bd203
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-25 18:04:25 +0100

    Update aula32_exercicio.py

--- aula32_exercicio.py
+++ aula32_exercicio.py
@@ -48,24 +48,31 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário
 descrito, exiba a saudação apropriada. Ex. 
 Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-hora = int(input('Qual horas são agora (0-23)'))
-if 0 <= hora <= 11:
-    print('Bom dia!')
-elif 12 <= hora <= 17:
-    print('Boa tarde!')
-else:
-    print('boa noite!')    
+try:
+    hora = int(input('Qual horas são agora (0-23)'))
+    if 0 <= hora <= 11:
+        print('Bom dia!')
+    elif 12 <= hora <= 17:
+        print('Boa tarde!')
+    else:
+        print('boa noite!')
+except:
+    print('Por favor, digite apenas números inteiros.')  
 """
 Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". """
 
-nome = input('Qual seu nome: ')
+nome = input('Qual seu nome: '))
 tamanho_nome = len(nome)
-print(tamanho_nome)
-if tamanho_nome <= 4:
-    print(f'seu nome {nome} é curto.')
-elif  4 < tamanho_nome <= 6:
-    print(f'Seu nome {nome} é normal.')    
-else:
-    print(f'Seu nome {nome} é muito grande.')    
+
+if tamanho_nome > 1:    
+    if tamanho_nome <= 4:
+            print(f'seu nome {nome} é curto.')
+    elif  4 < tamanho_nome <= 6:
+            print(f'Seu nome {nome} é normal.')    
+    else:
+            print(f'Seu nome {nome} é muito grande.')    
+else: 
+    print('Digite mais de uma letra.')
+
