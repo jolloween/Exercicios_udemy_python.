7f077a7cf7c062055732a4da2a045429259fc17f
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-25 22:15:40 +0100

    Add files via upload

---
+++ aula35_CondicoesEmDetalhes.py
@@ -0,0 +1,12 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição é verdadeira
+loop infinito: Quando uma condição não tem fim.
+"""
+contador = 0
+
+while contador < 10:
+    contador += 1
+    print(contador, end=" ")
+print('acabou!')    
