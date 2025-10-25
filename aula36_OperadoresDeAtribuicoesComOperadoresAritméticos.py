d918c7555e2f0dd610ede504ceb3164646716f65
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-25 22:30:01 +0100

    Add files via upload

---
+++ aula36_OperadoresDeAtribuicoesComOperadoresAritméticos.py
@@ -0,0 +1,42 @@
+"""
+Operdadores de Atribuições 
+= += -= *= /= //= **= %=
+"""
+# contador = 0
+
+# while contador < 10:
+#     contador = contador + 1
+#     print(contador, end=" ")
+# print('acabou!')    
+
+contador = 2
+   ####
+contador += 1
+print(contador)
+
+contador = 10
+   ####
+contador /= 2
+print(contador)
+
+contador = 9
+   ####
+contador //= 2
+print(contador)
+
+
+contador = 2
+   ####
+contador *= 3
+print(contador)
+
+
+contador = 2
+   ####
+contador **= 10
+print(contador)
+
+contador = 3
+   ####
+contador -= 1
+print(contador)
