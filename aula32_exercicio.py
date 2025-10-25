4161c5e4e4261daff9e401ffe85b18e75add8712
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-25 17:48:56 +0100

    Update aula32_exercicio.py

--- aula32_exercicio.py
+++ aula32_exercicio.py
@@ -3,16 +3,44 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou ímpar. Caso o usuário não digite um número
 inteiro, informe que não é um número inteiro.
 """
-usuario = input('Digite um número: ')
+entrada = input('Digite um número: ')
 
-if usuario.isdigit():
-    numero_inteiro = int(usuario)
-    if numero_inteiro % 2 ==0:
-        print(f'{numero_inteiro} é par.') 
+if entrada.isdigit():
+    par_impar = int(entrada)
+    if par_impar % 2 == 0:
+        print(f'{par_impar} é par.') 
     else:
-        print(f'{numero_inteiro} é ímpar.')    
+        print(f'{par_impar} é ímpar.')    
 else:
-    print('Você não digitou um número inteiro.')              
+    print('Você não digitou um número inteiro.') 
+
+# TODO OUTRO EXEMPLO
+entrada = input('Digite um número: ')      
+
+if entrada.isdigit():
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
+
+    if par_impar is True:
+        par_impar_texto = 'Par'
+    print(f'o númmero {entrada} é {par_impar_texto}')
+else: 
+    print('Você não digitou um número inteiro.')
+
+# TODO OUTRO EXEMPLO 2
+entrada = input('Digite um número: ')      
+
+try:
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'ímpar'
+
+    if par_impar is True:
+        par_impar_texto = 'Par'
+    print(f'o númmero {entrada} é {par_impar_texto}')
+except: 
+    print('Você não digitou um número inteiro.')
 
 
 """
@@ -32,7 +60,7 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". """
 
-nome = input('Qual seu nome:')
+nome = input('Qual seu nome: ')
 tamanho_nome = len(nome)
 print(tamanho_nome)
 if tamanho_nome <= 4:
@@ -41,4 +69,3 @@ elif  4 < tamanho_nome <= 6:
     print(f'Seu nome {nome} é normal.')    
 else:
     print(f'Seu nome {nome} é muito grande.')    
-
