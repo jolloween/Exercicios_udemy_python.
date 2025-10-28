bb0c054edad9bbd25a65cb33d7c0d3328696d94b
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-28 21:50:44 +0000

    Add second example of while-else usage

--- aula41_while_else.py
+++ aula41_while_else.py
@@ -13,3 +13,21 @@ while i < len(string):
 else:
     print('Não encontrei um espaço na string.')
 print('Fora do while.')
+
+""""Segundo exemplo"""
+
+
+string = 'Valorqualquer' #sem espaços
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break #como não tem espaços o else foi executado.
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
