aef8cb36b91b9913f6d52b4d9cd2b94a1257ebbb
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-10-29 22:05:17 +0000

    Add files via upload

--- aula48c_cuidadosComTiposMutaveis.py
+++ aula48c_cuidadosComTiposMutaveis.py
@@ -4,8 +4,13 @@ Cuidados com dados mutáveis
 = - aponta para o mesmo valor na memória (mutável)
 """
 lista_a = ['Luiz', 'Maria', 1, True, 1.2]
-lista_b = lista_a.copy()
-
+lista_b = lista_a
+lista_a[0] = 'Qualquer coisa'
+print(lista_a)
+print(lista_b)
+print("=-"*20)
+lista_a = ['Luiz', 'Maria', 1, True, 1.2]
+lista_b = lista_a.copy() #foi feito uma cópia da lista_a. neste caso a variável_b fica imutável.
 lista_a[0] = 'Qualquer coisa'
 print(lista_a)
 print(lista_b)
