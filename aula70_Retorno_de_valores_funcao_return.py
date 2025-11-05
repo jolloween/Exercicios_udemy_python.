ab2fc444543c5f9b3e936259cb0278789b1f77e0
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2025-11-05 11:49:48 +0000

    Add files via upload

---
+++ aula70_Retorno_de_valores_funcao_return.py
@@ -0,0 +1,14 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x, y):
+    return x + y
+    
+
+
+soma1 = soma(3, 5)
+soma2 = soma(4, 6)
+soma3 = soma(2, 1)
+print(soma1 + soma2 + soma3)
+
