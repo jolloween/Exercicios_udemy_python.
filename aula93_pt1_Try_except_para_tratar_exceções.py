cb4158279985345813fd0eb7f426ddb96c06cb27
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-01-28 22:49:04 +0000

    Add files via upload

---
+++ aula93_pt1_Try_except_para_tratar_exceções.py
@@ -0,0 +1,31 @@
+import os
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+limpar_tela()
+
+# Try, except, else, finally
+
+# (Parte1) try e except para tratar exceções
+# a = 18
+# b = 0
+# c = a / b
+
+try:
+    a = 18
+    b = 0
+    # print(b[0])
+    print('Linha 1'[1000])
+    c = a / b
+    print('Linha 2')
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome b não está definido')
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError')
+    print('msg:', error)
+    print('nome:', error.__class__.__name__)
+except Exception:
+    print('ERRO DESCONHECIDO.')
+
+print('CONTINUAR')
