4e4b740587c2959619bfe112568b628aa3952799
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-02-02 22:35:40 +0000

    Add files via upload

---
+++ aula99.py
@@ -0,0 +1,16 @@
+# from sys import path
+# import aula99_package
+# from aula99_package.modulo import soma
+# from aula99_package.modulo import *
+
+# print(soma(5, 6))
+
+# print(aula99_package.modulo.soma(3, 10))
+# print(variavel)
+# print(nova_variavel)
+
+from aula99_package.modulo import fala_oi
+
+fala_oi()
+
+# print(*path, sep='\n')
---
+++ modulo.py
@@ -0,0 +1,18 @@
+__all__ = [
+    'variavel',
+    'soma',
+    'nova_variavel'
+]
+
+variavel = 'João'
+numero = 123
+nova_variavel = 'Helloween'
+
+def soma(x, y):
+    return x + y
+
+
+from aula99_package.modulob import fala_oi
+
+# fala_oi()
+
