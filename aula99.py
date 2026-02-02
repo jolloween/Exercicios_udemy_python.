529417aea8e0e63a6fe3239f507777abf9c86c12
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-02-02 23:04:55 +0000

    Refactor aula99.py to enable function calls
    Uncommented function calls and added new imports.

--- aula99.py
+++ aula99.py
@@ -9,8 +9,16 @@
 # print(variavel)
 # print(nova_variavel)
 
-from aula99_package.modulo import fala_oi
+##from aula99_package.modulo import fala_oi
 
-fala_oi()
+#fala_oi()
 
 # print(*path, sep='\n')
+
+from aula99_package import soma, fala_oi, nova_variavel, dobra
+
+# print(aula99_package.dobra(2))
+print(soma(2, 4))
+print(dobra(2))
+fala_oi()
+print(nova_variavel)
