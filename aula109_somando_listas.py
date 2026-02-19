0b75cf7b652441d0c966bfe849133b41648de586
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-02-19 20:41:15 +0000

    Add files via upload

---
+++ aula109_somando_listas.py
@@ -0,0 +1,32 @@
+#TODO: Somando duas listas.
+
+"""
+Considerando duas listas de inteiros ou floats (lista A e lista B)
+Some os valores nas listas retornando uma nova lista com os valores somados:
+Se uma lista for maior que a outra, a soma só vai considerar o tamanho da
+menor.
+Exemplo:"""
+
+from itertools import zip_longest
+l1= [1, 2, 3, 4, 5, 6, 7]
+l2= [1, 2, 3, 4]
+lenght = len(l2)
+# lista_c = lista_a + lista_b
+lista_soma = []
+
+print(list(zip(l1, l2))) #União das duas listas usando o zip
+
+#1º forma:
+for i in range(len(l2)):
+    lista_soma.append(l1[i] + l2[i])
+print(lista_soma)
+
+#2º forma:
+
+for i, _ in enumerate(l2):
+    lista_soma.append(l1[i] + l2[i])
+print(lista_soma)
+
+#3º forma:
+lista_zip = [x + y for x, y in zip(l1, l2)]
+print(lista_zip)
