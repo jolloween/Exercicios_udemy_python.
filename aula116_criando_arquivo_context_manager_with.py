36ca3970b39390c4936424a9214c907ee8edd5ec
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-20 10:59:04 +0000

    Add files via upload

---
+++ aula116_criando_arquivo_context_manager_with.py
@@ -0,0 +1,75 @@
+import os
+# Criando arquivos com Python + Context Manager with
+# Usamos a função open para abrir
+# um arquivo em Python (ele pode ou não existir)
+# Modos:
+# r (leitura), w (escrita), x (para criação)
+# a (escreve ao final), b (binário)
+# t (modo texto), + (leitura e escrita)
+# Context manager - with (abre e fecha)
+# Métodos úteis
+# write, read (escrever e ler)
+# writelines (escrever várias linhas)
+# seek (move o cursor)
+# readline (ler linha)
+# readlines (ler linhas)
+# Vamos falar mais sobre o módulo os, mas:
+# os.remove ou unlink - apaga o arquivo
+# os.rename - troca o nome ou move o arquivo
+# Vamos falar mais sobre o módulo json, mas:
+# json.dump = Gera um arquivo json
+# json.load
+
+caminho_arquivo = 'C:\\Users\\jp-pe\\OneDrive\\Área de Trabalho\\curso_python\\curso_python\\venv\\Nova pasta atenção\\'
+caminho_arquivo += 'aula116.txt'
+
+# with open(caminho_arquivo, 'w') as arquivo:
+#     arquivo.write('JP\n')
+#     arquivo.write('go ahead\n')
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+########################################################
+
+# with open(caminho_arquivo, 'w+') as arquivo:
+#     print(type(arquivo))
+#     arquivo.write('linha 1\n')
+#     arquivo.write('linha 2\n')
+#     arquivo.writelines(
+#         ('linha 3\n', 'linha 4')
+#     )
+    
+#     arquivo.seek(0, 0)
+#     print(arquivo.read())
+#     print('lendo\n')
+#     arquivo.seek(0, 0)
+#     print(arquivo.readline(), end='')
+#     print(arquivo.readline().strip())
+#     print(arquivo.readline().strip())
+#     print('\nreadlines')
+#     arquivo.seek(0, 0)
+#     for linha in arquivo.readlines():
+#         print(linha.strip())
+
+
+# print('#' *10)
+
+# with open(caminho_arquivo, 'r') as arquivo:
+#     print(arquivo.read())
+# ########################################################
+# print('#' *10)
+
+
+with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+    arquivo.write('Atenção\n')
+    arquivo.write('linha 2\n')
+    arquivo.writelines(
+                    ('linha 3\n', 'Linha 4\n'),
+                    
+    )
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print(arquivo.read())
+
+os.remove(caminho_arquivo) #remove o arquivo
+os.rename(caminho_arquivo, 'aula116-2.txt') #renomea o arquivo (novo caminho)
