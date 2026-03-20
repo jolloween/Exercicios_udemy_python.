5d5dfbd963a4339b2e3141d0152ae1c14dab40ed
Hi there! I'm João Paulo <jp-pedrosa@hotmail.com>
2026-03-20 11:34:59 +0000

    Add files via upload

--- aula117/aula117_json.py
+++ aula117/aula117_json.py
@@ -1,1 +1,25 @@
+import json
+# pessoa = {
+#     'nome': 'João Paulo',
+#     'sobrenome': 'Pedrosa Soares',
+#     'enderecos': [
+#         {'rua': 'R1', 'numero': 32},
+#         {'rua': 'R2', 'numero': 55},
+#     ],
+#     'altura': 1.8,
+#     'numeros preferidos': (2,4,6,8,7),
+#     'dev': True,
+#     'nada': None,
+# }
 
+# with open('aula117_json.json', 'w', encoding='utf-8') as arquivo:
+#     json.dump(
+#         pessoa,
+#         arquivo,
+#         ensure_ascii=False,
+#         indent=2,
+#         )
+
+with open('aula117_json.json', 'r', encoding='utf-8') as arquivo:
+    pessoa = json.load(arquivo)
+    print(pessoa)
